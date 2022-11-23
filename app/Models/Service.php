ac9c3d8dfe749f8b06ad4fde40f13b2203189a4a
Khen Christian Vargas <khenchristian.vargas@tup.edu.ph>
2022-11-23 11:39:53 +0800

    added service model and migration

---
+++ app/Models/Service.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'service_name', 'description', 'price'
+    ];
+
+}
---
+++ database/migrations/2022_11_23_033646_create_services_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('service_name');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->text('img_path')->default('/storage/images/default.jpg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+};
