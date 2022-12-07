0fa7e5bc7e9d31a7371795500c8f6e323f34c367
Khen Vargas <khenchristian.vargas@tup.edu.ph>
2022-12-07 13:11:47 +0800

    login now receives formdata(insomnia)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,6 +8,34 @@ const saltRounds = 10;
 const fs = require("fs");
 const { handleLogin } = require("../controller/authController");
 
-router.post("/", handleLogin);
+const FILE_TYPE_MAP = {
+    "image/png": "png",
+    "image/jpeg": "jpeg",
+    "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error("invalid image type");
+
+        if (isValid) {
+            uploadError = null;
+        }
+        cb(uploadError, "public/uploads");
+    },
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.split(" ").join("-");
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${extension}`);
+    },
+});
+
+const uploadOptions = multer({
+    storage: storage,
+});
+
+// router.post("/", handleLogin);
+router.post("/", uploadOptions.single("uploads"), handleLogin);
 
 module.exports = router;
